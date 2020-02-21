bead2e52612249f32f2c494f264bb8e2ec2861ab
Pawan Kolhe <contact@pawankolhe.com>
2020-02-22 02:26:13 +0530

    add background images

--- src/components/movie-listing/MovieListing.vue
+++ src/components/movie-listing/MovieListing.vue
@@ -2,6 +2,9 @@
   <div class="movie-listing section">
     <div class="container">
       <omdb-movie-card v-for="(movie, index) in getMovies" :key="index" :title="movie.Title" :poster="movie.Poster" :year="movie.Year" :imdbID="movie.imdbID" />
+      <img class="backimage1" src="../../assets/icons/film.svg">
+      <img class="backimage2" src="../../assets/icons/clapperboard.svg">
+      <img class="backimage3" src="../../assets/icons/cinema.svg">
     </div>
   </div>
 </template>
@@ -31,5 +34,31 @@ export default {
   justify-content: center;
   align-items: stretch;
   grid-gap: 35px 6%;
+  position: relative;
+}
+
+.backimage1 {
+  position: absolute;
+  top: 100px;
+  left: -250px;
+  opacity: 0.03;
+  width: 200px;
+  height: auto;
+}
+.backimage2 {
+  position: absolute;
+  top: 500px;
+  right: -250px;
+  opacity: 0.03;
+  width: 200px;
+  height: auto;
+}
+.backimage3 {
+  position: absolute;
+  top: 1000px;
+  left: -250px;
+  opacity: 0.03;
+  width: 200px;
+  height: auto;
 }
 </style>
